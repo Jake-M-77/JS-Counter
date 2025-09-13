b9a3a9d2a61de0341a9bf6b685c31ed919990099
Jake M <jakemanson77@icloud.com>
2025-09-13 16:51:51 +0100

    refactor: convert counter button handlers to arrow functions

--- index.js
+++ index.js
@@ -4,20 +4,9 @@ function updateCounter(){
     counterText.textContent = counter;
 }
 
+document.getElementById('numberIncrease').onclick = () => updateCounter(counter++);
 
-document.getElementById('numberIncrease').onclick = function () {
-    counter++;
-    updateCounter();
-}
+document.getElementById('numberDecrease').onclick = () => updateCounter(counter--);
 
-document.getElementById('numberDecrease').onclick = function () {
-    counter--;
-    updateCounter();
+document.getElementById('numberReset').onclick = () => updateCounter(counter = 0)
 
-}
-
-document.getElementById('numberReset').onclick = function () {
-    counter = 0;
-    updateCounter();
-
-}
